1004e27b4f97fa6b9f123274f51510f711cd7776
Takashi Mori <kanetugu2015@gmail.com>
2024-03-25 15:57:14 +0900

    add utils func

--- plugin-srcs/Assets/Scripts/HakoRoboParts/Interface/IRobotPartsConfig.cs
+++ plugin-srcs/Assets/Scripts/HakoRoboParts/Interface/IRobotPartsConfig.cs
@@ -27,6 +27,45 @@ namespace Hakoniwa.PluggableAsset.Assets.Robot.Parts
         public IoMethod io_method = IoMethod.RPC;
         public IoDir io_dir = IoDir.WRITE;
         public RobotPartsConfig value = new RobotPartsConfig();
+
+        //RosTopicMessageConfig Conversion from RoboPartsConfigData
+        public static RosTopicMessageConfig[] getRosConfig(RoboPartsConfigData[] pcfg, string[] topic_name, string[] topic_type, int[] update_cycles)
+        {
+            RosTopicMessageConfig[] cfg = new RosTopicMessageConfig[pcfg.Length];
+            for (int i = 0; i < pcfg.Length; i++)
+            {
+                if (update_cycles == null)
+                {
+                    cfg[i] = getRosConfigSingle(pcfg[i], topic_name[i], topic_type[i], 1);
+                }
+                else
+                {
+                    cfg[i] = getRosConfigSingle(pcfg[i], topic_name[i], topic_type[i], update_cycles[i]);
+                }
+            }
+
+            return cfg;
+        }
+        public static RosTopicMessageConfig getRosConfigSingle(RoboPartsConfigData pcfg, string topic_name, string topic_type, int update_cycle)
+        {
+            RosTopicMessageConfig cfg = new RosTopicMessageConfig();
+            cfg.topic_message_name = topic_name;
+            cfg.topic_type_name = topic_type;
+            if (pcfg.io_dir == IoDir.READ)
+            {
+                cfg.sub = true;
+            }
+            else
+            {
+                cfg.sub = false;
+            }
+            cfg.pub_option = new RostopicPublisherOption();
+            cfg.pub_option.cycle_scale = update_cycle;
+            cfg.pub_option.latch = false;
+            cfg.pub_option.queue_size = 1;
+            return cfg;
+        }
+
     }
     [System.Serializable]
     public class RobotPartsConfig
