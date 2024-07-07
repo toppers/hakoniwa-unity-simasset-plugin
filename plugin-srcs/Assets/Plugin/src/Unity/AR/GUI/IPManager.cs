ed3275679de989a7915149dc7a5b9bf2c6f78011
Takashi Mori <kanetugu2015@gmail.com>
2024-07-08 06:24:59 +0900

    add fields

--- plugin-srcs/Assets/Plugin/src/Unity/AR/GUI/IPManager.cs
+++ plugin-srcs/Assets/Plugin/src/Unity/AR/GUI/IPManager.cs
@@ -6,11 +6,18 @@ public class IPManager : MonoBehaviour
 {
     public InputField server_ipInputField;
     public InputField client_ipInputField;
+    public InputField session_positionField;
+    public InputField session_rotationField;
+    public InputField session_originField;
+
     public Button saveButton;
     public Button startButton;
     public GameObject savePopupPanel;
     private string server_savedIPKey = "server_savedIP";
     private string client_savedIPKey = "client_savedIP";
+    private string session_savedPosKey = "session_savedPos";
+    private string session_savedRotKey = "session_savedRot";
+    private string session_savedOriginKey = "session_savedOrigin";
 
     void Start()
     {
@@ -19,6 +26,15 @@ public class IPManager : MonoBehaviour
         server_ipInputField.text = server_savedIP;
         string client_savedIP = PlayerPrefs.GetString(client_savedIPKey, "127.0.0.1:54001");
         client_ipInputField.text = client_savedIP;
+
+        string session_savedPos = PlayerPrefs.GetString(session_savedPosKey, "0,0,0");
+        session_positionField.text = session_savedPos;
+        string session_savedRot = PlayerPrefs.GetString(session_savedRotKey, "0,0,0");
+        session_rotationField.text = session_savedRot;
+
+        string session_saveOrigin = PlayerPrefs.GetString(session_savedOriginKey, "0,0,0");
+        session_originField.text = session_saveOrigin;
+
 
         // ボタンのクリックイベントにメソッドを登録
         saveButton.onClick.AddListener(SaveIP);
@@ -35,6 +51,15 @@ public class IPManager : MonoBehaviour
         PlayerPrefs.SetString(server_savedIPKey, server_ipToSave);
         string client_ipToSave = client_ipInputField.text;
         PlayerPrefs.SetString(client_savedIPKey, client_ipToSave);
+
+        string session_savedPos = session_positionField.text;
+        PlayerPrefs.SetString(session_savedPosKey, session_savedPos);
+        string session_savedRot = session_rotationField.text;
+        PlayerPrefs.SetString(session_savedRotKey, session_savedRot);
+
+        string session_saveOrigin = session_originField.text;
+        PlayerPrefs.SetString(session_savedOriginKey, session_saveOrigin);
+
         PlayerPrefs.Save();
 
         // ポップアップパネルを表示
